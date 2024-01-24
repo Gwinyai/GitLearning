d24042e37bb7c38a3de64a3933a177664fd32221
Gwinyai Nyatsoka <gnyatsoka@gmail.com>
2024-01-24 15:18:39 +0200

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Gwinyai Nyatsoka on 24/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
